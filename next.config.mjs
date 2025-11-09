4b94e26c9d27f709093c129dc13035041bf42946
azzm04 <azzamsyaifull@gmail.com>
2025-11-09 21:57:29 +0700

    refactor

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,5 @@ const nextConfig = {
 export default withPWA({
   dest: 'public',
   register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  skipWaiting: true
 })(nextConfig);
